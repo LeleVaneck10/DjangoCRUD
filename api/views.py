2095b70c8ae6af0976f80e28bcf0114a12eb9a1d
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2022-10-13 15:43:38 +0100

    first commit

---
+++ api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view    
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List': '/product-list/',
+        'Detail View': '/product-detail/<int:id>/',
+        'Create': '/product-create/',
+        'Update': '/product-update/<int:id>/',
+        'Delete': '/product-detail/<int:id>/',
+    }
+    
+    return Response(api_urls);
